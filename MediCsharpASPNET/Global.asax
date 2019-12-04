b0701b652cf6d073a079c4aa0b7c26b00aa01b69
dalvarenga97 <diana_belen92@hotmail.com>
2019-12-04 19:32:16 -0300

    Agregar archivos de proyecto.

---
+++ MediCsharpASPNET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MediCsharpASPNET.MvcApplication" Language="C#" %>
---
+++ MediCsharpASPNET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MediCsharpASPNET.Startup))]
+namespace MediCsharpASPNET
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
